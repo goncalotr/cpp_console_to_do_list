063e3165fe6216381c3dea8eb6cffea16599a2ec
goncalotr <goncalotribeiro@gmail.com>
2025-05-19 23:09:10 +0100

    feat(tasks): add mark tasks as complete

--- src/main.cpp
+++ src/main.cpp
@@ -80,12 +80,15 @@ int main (int argc, char** argv) {
 				break;
 
 			case 3:
+
+				// no tasks
 				if (tasks.empty()) {
 					std::cout << "No tasks to mark. Add some tasks first!\n";
 					waitForEnter();
 					break;
 				}
 
+				// task list
 				std::cout << "\n--- Mark Task as Completed ---\n";
 				for (size_t i = 0; i < tasks.size(); ++i) {
 					const Task currentTask = tasks[i];
@@ -99,6 +102,40 @@ int main (int argc, char** argv) {
 				}
 				std::cout << "-----------------------------\n";
 
+				// input operation number
+				int taskNumber;
+				std::cout << "Enter the number of the task to mark as completed (or 0 to cancel): ";
+				std::cin >> taskNumber;
+				if (std::cin.fail()) {
+					std::cout << "Invalid input. Please enter a number. \n";
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					waitForEnter();
+					break;
+				}
+
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				if (taskNumber == 0) {
+					std::cout << "Operation cancelled.\n";
+					waitForEnter();
+					break;
+				}
+
+				// validate option
+				if (taskNumber > 0 && static_cast<size_t>(taskNumber) <= tasks.size()) {
+
+					// non-const reference because the task will be modified
+					Task& taskToMark = tasks[taskNumber - 1];
+
+					if (taskToMark.getCompleted()) {
+						std::cout << "Task \"" << taskToMark.getDescription() << "\" is already marked as completed. \n";
+					} else {
+						taskToMark.setComplete();
+						std::cout << "Task \"" << taskToMark.getDescription() << "\" marked as completed! \n";
+					}
+				} else {
+					std::cout << "Invalid task number. Please enter a number from the list. \n";
+				}
 
 				waitForEnter();
 				break;
