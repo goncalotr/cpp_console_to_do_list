9b6797b95aa6f998340f0c4d2e4dfa7a2da929e5
goncalotr <goncalotribeiro@gmail.com>
2025-05-20 13:21:46 +0100

    fix(main): fix task remove function call

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,6 @@ int main (int argc, char** argv) {
 		}
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-		int taskNumber;
 		switch (choice) {
 
 			// add task
@@ -57,48 +56,7 @@ int main (int argc, char** argv) {
 
 			// remove tasks
 			case 4:
-
-				// no tasks
-				if (tasks.empty()) {
-					std::cout << "No tasks to remove. Add some tasks first!\n";
-					waitForEnter();
-					break;
-				}
-
-				// task list
-				std::cout << "\n--- Select Task to Remove ---\n";
-				displayTasks(tasks);
-
-				// input operation number
-				//int taskNumber;
-				std::cout << "Enter the number of the task to remove (or 0 to cancel): ";
-				std::cin >> taskNumber;
-				if (std::cin.fail()) {
-					std::cout << "Invalid input. Please enter a number. \n";
-					std::cin.clear();
-					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-					waitForEnter();
-					break;
-				}
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-				if (taskNumber == 0) {
-					std::cout << "Operation cancelled.\n";
-					waitForEnter();
-					break;
-				}
-
-				if (taskNumber > 0 && static_cast<size_t>(taskNumber) <= tasks.size()) {
-					size_t indexToRemove = static_cast<size_t>(taskNumber - 1);
-					std::string removedTaskDescription = tasks[indexToRemove].getDescription();
-					tasks.erase(tasks.begin() + indexToRemove);
-
-					std::cout << "Task \"" << removedTaskDescription << "\" removed successfully!\n";
-				} else {
-					std::cout << "Invalid task number. No task removed. \n";
-				}
-
-				waitForEnter();
+				handleRemoveTask(tasks);
 				break;
 
 			// save tasks
